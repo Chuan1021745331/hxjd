2ac8782478073d84fd72a86375bcccdc422f5bfa
md <123456>
2018-01-29 09:59:19 +0800

    线路跳转工点

--- HXJD/HXJD-web/src/main/webapp/common/js/route/circuitSel.js
+++ HXJD/HXJD-web/src/main/webapp/common/js/route/circuitSel.js
@@ -1,11 +1,38 @@
 /**
  * Created by hxjd on 2018/1/26.
  */
-layui.use('element', function(){
+layui.use(['form','layer','element'], function(){
     var element = layui.element;
-
+    initSizeHeight($("#circuit_root_div"));
     //监听折叠
     element.on('test', function(data){
         layer.msg('展开状态：'+ data.show);
     });
+    /*工点按钮事件*/
+    $(".wbutton").on("click",function () {
+        alert("wbutton")
+        var adata = $(this).attr("wdata");
+         var iframe = window.parent.document.getElementsByClassName("tbmiframe")[0];
+         var url = bpath+"/route/workSiteSel?wid=" + adata;
+         console.log("--------------="+iframe);
+         $(iframe).attr("src", url);
+    });
+    /**
+     * 初始化页面大小
+     * @param $obj
+     * @param isIndex
+     */
+    function initSizeHeight($obj, isIndex)
+    {
+        // var width = document.documentElement.clientWidth;
+        var height = $(window).height();
+        if(isIndex)
+        {
+            $obj.css({ height: height - 60});//112是控制栏
+        }
+        else
+        {
+            $obj.css({height:height});
+        }
+    }
 });
