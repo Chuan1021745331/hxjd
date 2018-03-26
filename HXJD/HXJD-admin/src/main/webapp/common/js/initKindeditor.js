fd2c3eed42c527fd386e05cc1b0369bfede1beb7
JackSonChuan <1021745331@qq.com>
2018-03-26 13:42:35 +0800

    细节调整

--- HXJD/HXJD-admin/src/main/webapp/common/js/initKindeditor.js
+++ HXJD/HXJD-admin/src/main/webapp/common/js/initKindeditor.js
@@ -1,14 +1,14 @@
 
 var KindeditorOption={
     items:[
-        'source', '|', 'undo', 'redo', '|', 'preview', 'print', 'template', 'code', 'cut', 'copy', 'paste',
+        'source', '|', 'undo', 'redo', '|', 'preview', 'print','code', 'mytemplate','cut', 'copy', 'paste',
         'plainpaste', 'wordpaste', '|', 'justifyleft', 'justifycenter', 'justifyright',
         'justifyfull', 'insertorderedlist', 'insertunorderedlist', 'indent', 'outdent', 'subscript',
         'superscript', 'clearhtml', 'quickformat', 'selectall', '|', 'fullscreen', '/',
-        'formatblock', 'fontname', 'fontsize', '|', 'forecolor', 'hilitecolor', 'bold',
-        'italic', 'underline', 'strikethrough', 'lineheight', 'removeformat', '|', 'image', 'multiimage',
+        'formatblock', 'fontname', 'fontsize', '|', 'forecolor', 'hilitecolor', 'bol' +
+        'age', 'multiimage',
         'flash', 'media', 'insertfile', 'table', 'hr', 'emoticons', 'baidumap', 'pagebreak',
-        'anchor', 'link', 'unlink', '|', 'mytemplate','about'
+        'anchor', 'link', 'unlink', '|','about'
     ]
 }
 KindEditor.lang({
